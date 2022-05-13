666a4bc192ac43c04073cdc686bfaac36c6d546a
Kali_Linux <100436390+WhiteDevil1810@users.noreply.github.com>
2022-05-13 23:36:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// const arr = ["John", "orange", "pineapple", "banana", "grapes"];
+function getrandom() {
+    //   const randomValue = circle[Math.floor(Math.random() * circle.length)];
+    //   document.querySelector(".content").textContent = circle;
+      document.write(circle);
+    }
+    
+    const circle = document.getElementsByClassName(".one").innerHTML;
+    // (circle);
